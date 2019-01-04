37b515ec23a92c5dc61c49129534ce616b35d521
titusefferian <kiritosato@gmail.com>
2019-01-04 18:28:39 +0700

    add dynamo db

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 const serverless = require('serverless-http')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const AWS = require('aws-sdk')
 
-app.get('/',(req,res)=>{
+const USERS_TABLE = process.env.USERS_TABLE
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+app.use(bodyParser.json({ strict: false }))
+app.get('/', (req, res) => {
     res.send({
-        success:true,
-        data:'serverless'
+        success: true,
+        data: 'serverless'
+    })
+})
+
+app.get('/users/:userId', function (req, res) {
+    const params = {
+        TableName: USERS_TABLE,
+        Key: {
+            userId: req.params.userId,
+        },
+    }
+
+    dynamoDb.get(params, (error, result) => {
+        if (error) {
+            console.log(error);
+            res.status(400).json({ error: 'Could not get user' });
+        }
+        if (result.Item) {
+            const { userId, name } = result.Item;
+            res.json({ userId, name });
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
+    });
+})
+
+app.post('/users', (req, res) => {
+    const { userId, name } = req.body
+    if (typeof userId !== 'string') {
+        res.status(400).json({
+            error: 'userid must be string'
+        })
+    }
+    else if (typeof name !== 'string') {
+        res.status(400).json({
+            error: 'name must be string'
+        })
+    }
+    const params = {
+        TableName: USERS_TABLE,
+        Item: {
+            userId: userId,
+            name: name
+        }
+    }
+
+    dynamoDb.put(params, (error) => {
+        if (error) {
+            console.log(error)
+            res.status(400).json({
+                error: 'could not create user'
+            })
+        }
+        res.json({ userId, name })
     })
 })
 
